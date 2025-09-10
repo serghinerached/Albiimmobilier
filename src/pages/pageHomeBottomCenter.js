cfa92ad290b24eb35e8b0aa3cb80f542e03799cb
Rached SERGHINE <Rached.Serghine@dsi-ap.com>
2025-09-10 15:54:34 +0200

    10/09

--- src/pages/pageHomeBottomCenter.js
+++ src/pages/pageHomeBottomCenter.js
@@ -2,6 +2,8 @@ import {styles} from '../components/ComponentCss';
 import {useState,useEffect} from "react";
 import fDataTableAnnonce from '../components/ReadSupabase/loadSupabaseDatas';
 import { supabase } from '../components/ReadSupabase/supabaseClient';
+import photo from '../photos/1-1.png'; // chemin depuis src
+
 
 function DivPageHomeBottomCenter() {
   const [rows, setRows] = useState([]);
@@ -26,31 +28,24 @@ function DivPageHomeBottomCenter() {
 
   return (
           rows.map((row) => (
-            <table style={{border:"2px solid black",marginBottom:"20px",fill:"black",width:"85%"}} >
+            <table style={{border:"2px solid black",marginBottom:"20px",fill:"black",width:"90%"}} >
               <tr style={{color:"white",backgroundColor:"Steelblue"}}>
                 <td style={{padding:"5px"}}>{row.type}</td>
-                <td colSpan={4} align='right'>{`${row.prix} €`}</td>
+                <td colSpan={4} align='right' style={{paddingRight:"10px"}}>{`${row.prix} €`}</td>
               </tr>
               <tr style={{color:"black",backgroundColor:"LightSteelblue"}}>
                 <td style={{padding:"5px"}}>
-                  <label for="created">Paru le</label><br></br>
-                  <input type="text" name="created" id="chauffage" value= {new Date(row.created).toLocaleDateString()} disabled="disabled" size="7"
-                   style={{textAlign:"center"}}/>
+                  Paru le {new Date(row.created).toLocaleDateString()}
+                 
                 </td>
-                <td>
-                  <label for="chauffage">Chauffage</label><br></br>
-                  <input type="text" name="chauffage" id="chauffage" value={row.chauffage} disabled="disabled" size="6"  style={{textAlign:"center"}}/>
-                </td>
-                 <td>
-                  <label for="pieces">Pieces</label><br></br>
-                  <input type="text" name="pieces" id="chpiecesauffage" value={row.pieces} disabled="disabled" size="1"  style={{textAlign:"center"}}/>
-                </td>
-                 <td>
-                  <label for="superficie">Superficie</label><br></br>
-                  <input type="text" name="superficie" id="superficie" value={`${row.superficie} m2`}   disabled="disabled" size="5"  style={{textAlign:"center"}} />
-                </td>
-                <td>
-                  photo
+                
+                <td style={{
+    backgroundImage: `url(${photo})`,
+    backgroundSize: "cover",      // ou "contain" selon l'effet voulu
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center", width: "200px",               // largeur de la cellule
+    height: "100px"}}>
+                  
                 </td>
               </tr>
               
